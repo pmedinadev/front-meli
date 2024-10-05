a6b2d54ceeb68323c0a290e0b006987fedc55640
Jesús Pablo Medina García <jp.medina20@info.uas.edu.mx>
2024-10-05 16:41:44 -0700

    Adición de carrusel en home

--- src/app/(app)/page.js
+++ src/app/(app)/page.js
@@ -1,11 +1,42 @@
+import Image from "next/image"
+import { Carousel, CarouselItem } from "react-bootstrap"
+
 export const metadata = {
   title: 'Mercado Libre',
 }
 
+const items = [
+  {
+    src: '/images/ads/ad1.webp',
+    alt: 'First slide',
+  },
+  {
+    src: '/images/ads/ad2.webp',
+    alt: 'Second slide',
+  },
+  {
+    src: '/images/ads/ad3.webp',
+    alt: 'Third slide',
+  },
+]
+
 const Home = () => {
   return (
     <>
-      <h1 className="text-center mt-4">Mercado Libre Homepage</h1>
+      {/* Carrusel de anuncios */}
+      <Carousel>
+        {items.map((item, index) => (
+          <CarouselItem key={index}>
+            <Image
+              src={item.src}
+              alt={item.alt}
+              width={1920}
+              height={1080}
+              className="carousel-image"
+            />
+          </CarouselItem>
+        ))}
+      </Carousel>
     </>
   )
 }
