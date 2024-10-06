95b4a7e1bfee7de503ba1a57099b50da6b7f75fb
Jesús Pablo Medina García <jp.medina20@info.uas.edu.mx>
2024-10-05 20:04:15 -0700

    Sección Productos

--- src/app/(app)/page.js
+++ src/app/(app)/page.js
@@ -1,5 +1,12 @@
-import Image from "next/image"
-import { Carousel, CarouselItem } from "react-bootstrap"
+import Image from 'next/image'
+import {
+  Card,
+  CardBody,
+  CardImg,
+  Carousel,
+  CarouselItem,
+  Container,
+} from 'react-bootstrap'
 
 export const metadata = {
   title: 'Mercado Libre',
@@ -37,6 +44,106 @@ const Home = () => {
           </CarouselItem>
         ))}
       </Carousel>
+
+      {/* Productos */}
+      <Container>
+        <div className="bg-body rounded shadow-sm p-4 mt-4">
+          <h4>Productos</h4>
+
+          <div className="d-flex mt-3">
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+
+            <Card border="0" style={{ width: '12rem' }}>
+              <CardImg
+                src="/images/products/ejemplo.webp"
+                as={Image}
+                width={100}
+                height={180}
+              />
+              <CardBody>
+                <p>Teléfono Celular Samsung</p>
+                <h3 className='mb-0'>$2,000</h3>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </Container>
     </>
   )
 }
